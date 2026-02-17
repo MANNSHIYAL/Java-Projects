875d7d54db4c8ddc8cd406b7443b556f54be0cea
Mann <mannjshiyal@gmail.com>
2026-02-17 22:16:04 +0530

    FixedWindow algorithm.

---
+++ RateLimiter/RateLimiters.java
@@ -0,0 +1,21 @@
+
+public class RateLimiters {
+    public static void main(String[] args) throws InterruptedException {
+
+        FixedWindow fixedWindow = new FixedWindow(5);
+
+        for (int i = 0; i < 20; i++) {
+            printData("Fixed Window", fixedWindow.isRequestAllowed());
+            System.out.println("--------------------------------");
+            Thread.sleep(100);
+        }
+    }
+
+    private static void printData(String name, boolean isAllowed){
+        System.out.println(
+            String.format("%-20s : %s",
+            name,
+            isAllowed ? "ALLOWED" : "REJECTED")
+        );
+    }
+}
